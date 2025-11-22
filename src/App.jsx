1155a264e0a4e9848e803d59474f2ada6e26ee0a
shivusoumya <shivadatacamp@gmail.com>
2025-11-22 11:59:58 +0530

    portfolio navbar changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "#components/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main className="App">
+            <Navbar />
+        </main>
+    )
+}
+export default App
